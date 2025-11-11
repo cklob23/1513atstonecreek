8d81ba7dd6b85c4d592bbc30f30c750a6e15f182
cklob23 <klobe.caleb23@gmail.com>
2025-11-10 20:38:13 -0500

    Update email function

--- app/api/sendEmail.js
+++ app/api/sendEmail.js
@@ -9,24 +9,34 @@ export default async function handler(req, res) {
 
     try {
         const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
+            host: "imap.gmail.com",
             port: 465,
             secure: true,
             auth: {
                 user: "1513atstonecreek.inquiries@gmail.com",
                 pass: "plzz hsph kxrg qgyt",
             },
+            logger: true,
+            debug: true,
         });
 
-        await transporter.sendMail({
+        const info = {
             from: `"1513 at Stone Creek" <1513atstonecreek.inquiries@gmail.com>`,
             to: "info@1513atstonecreek.com",
             replyTo: email,
             subject: `New Inquiry from ${name}`,
             html: htmlBody,
-        });
-
-        return res.status(200).json({ success: true });
+        };
+        const mailInfo = await transporter.sendMail(info, function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`Email sent successfully.`);
+                console.log(mailInfo);
+                return res.status(200).json({ success: true });
+            }
+        })
+        
     } catch (error) {
         console.error("Email error:", error);
         return res.status(500).json({ success: false, error: "Email failed to send" });
